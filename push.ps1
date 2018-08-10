bc610773bfdbfc40aa964c1a51a4077c48c1bb12
Gabor Korosi <gabor.korosi@valtech.com>
2018-08-10 10:48:54 +0200

    10-08-2018 10:48

--- push.ps1
+++ push.ps1
@@ -1,6 +1,7 @@
 #!/bin/sh
 $Filepath = 'C:\AutoGitTest'
+$TimeAndDate = Get-Date -Format g
 Get-ChildItem -Path $Filepath -Recurse
 git add --all
-git commit -am "Regular auto-commit"
+git commit -am "$TimeAndDate"
 git push
