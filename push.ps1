10ea1e1947444c2d33fa524abaed3c1e67e39f03
unknown <DK-WIN-GAKO$@dk.valtech.com>
2018-08-14 15:02:39 +0200

    8/14/2018 3:02 PM

--- push.ps1
+++ push.ps1
@@ -1,4 +1,6 @@
 #!/bin/sh
+Push-Location $PSScriptRoot
+
 $Filepath = 'C:\AutoGitTest'
 $TimeAndDate = Get-Date -Format g
 Get-ChildItem -Path $Filepath -Recurse
@@ -6,3 +8,5 @@ Get-ChildItem -Path $Filepath -Recurse
 git add --all
 git commit -am "$TimeAndDate"
 git push
+
+Pop-Location
