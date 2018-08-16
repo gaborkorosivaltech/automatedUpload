ce3e10f9a83cecec924ce4ff4f538dfe0089cad4
Gabor Korosi <gabor.korosi@valtech.com>
2018-08-16 13:48:03 +0200

    16-08-2018 13:48

--- push - Copy.ps1
+++ push - Copy.ps1
@@ -1,4 +1,4 @@
-﻿
+﻿$code = {
 #!/bin/sh
 
 Push-Location $PSScriptRoot
@@ -7,10 +7,11 @@ $Filepath = 'C:\AutoGitTest'
 $TimeAndDate = Get-Date -Format g
 Get-ChildItem -Path $Filepath -Recurse
 
-
-
 git add --all
 git commit -am "$TimeAndDate"
 git push
 
 Pop-Location
+}
+
+[convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($code))
